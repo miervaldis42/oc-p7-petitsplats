30852b26e3b8392af40da6fddd944e9a53dd99df
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-02-25 14:11:41 +0400

    feat (client) : Filter Types
    Create new types to structure filter data

--- src/types/filterTypes.ts
+++ src/types/filterTypes.ts
@@ -19,6 +19,8 @@ type ApplianceFilterOptionsType = Array<ApplianceType>;
  */
 type UstensilFilterOptionsType = Array<UstensilType>;
 
+type FilterOptionValueType = ApplianceType | UstensilType | string;
+
 /**
  * @type
  * @description Object containing the arrays which will be used as options in the 3 recipe filters _(ingredients, appliances, ustensils)_
@@ -27,15 +29,29 @@ type UstensilFilterOptionsType = Array<UstensilType>;
  * @property {ApplianceFilterOptionsType} appliancesOptions
  * @property {UstensilFilterOptionsType} ustensilsOptions
  */
-interface FiltersType {
+interface FilterOptionsType {
   ingredientsOptions: IngredientFilterOptionsType;
   appliancesOptions: ApplianceFilterOptionsType;
   ustensilsOptions: UstensilFilterOptionsType;
+}
+
+type FilterType = "ingredient" | "appliance" | "ustensil";
+type FiltersType = Array<FilterType>;
+
+type ActionType = "add" | "remove";
+interface FilterInfoType {
+  type: FilterType;
+  tag: ApplianceType | UstensilType | string;
 }
 
 export type {
   IngredientFilterOptionsType,
   ApplianceFilterOptionsType,
   UstensilFilterOptionsType,
+  FilterOptionValueType,
+  FilterOptionsType,
+  FilterType,
   FiltersType,
+  ActionType,
+  FilterInfoType,
 };
